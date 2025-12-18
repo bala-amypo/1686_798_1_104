2564ad7df8a3a7e223407fd77b44014450ab7936
bala-amypo <bala@amypo.in>
2025-12-18 08:21:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/IssuedDeviceRecord.java
+++ src/main/java/com/example/demo/entity/IssuedDeviceRecord.java
@@ -6,7 +6,6 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "issued_device_records")
 public class IssuedDeviceRecord {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -38,12 +37,27 @@ public class IssuedDeviceRecord {
     public Long getId() {
          return id; 
          }
-    public Long getEmployeeId() { return employeeId; }
-    public void setEmployeeId(Long employeeId) { this.employeeId = employeeId; }
-    public Long getDeviceItemId() { return deviceItemId; }
-    public void setDeviceItemId(Long deviceItemId) { this.deviceItemId = deviceItemId; }
-    public LocalDate getIssuedDate() { return issuedDate; }
-    public LocalDate getReturnedDate() { return returnedDate; }
-    public void setReturnedDate(LocalDate returnedDate) { this.returnedDate = returnedDate; }
-    public String getStatus() { return status; }
+    public Long getEmployeeId() { 
+        return employeeId;
+         }
+    public void setEmployeeId(Long employeeId) {
+         this.employeeId = employeeId;
+          }
+    public Long getDeviceItemId() { 
+    return deviceItemId; 
+    }
+    public void setDeviceItemId(Long deviceItemId) { 
+        this.deviceItemId = deviceItemId; 
+        }
+    public LocalDate getIssuedDate() {
+         return issuedDate; }
+    public LocalDate getReturnedDate(){ 
+        return returnedDate;
+     }
+    public void setReturnedDate(LocalDate returnedDate) {
+         this.returnedDate = returnedDate; 
+         }
+    public String getStatus() {
+         return status; 
+         }
 }
