5a3867aed4df690fdf0602823d6d4051924b35fc
bala-amypo <bala@amypo.in>
2025-12-19 10:28:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/IssuedDeviceRecordServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.demo.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.IssuedDeviceRecord;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.IssuedDeviceRecordRepository;
+
+@Service
+public class IssuedDeviceRecordServiceImpl
+        implements IssuedDeviceRecordService {
+
+    private final IssuedDeviceRecordRepository repository;
+
+    public IssuedDeviceRecordServiceImpl(
+            IssuedDeviceRecordRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public IssuedDeviceRecord issueDevice(IssuedDeviceRecord record) {
+        record.setIssuedDate(LocalDate.now());
+        record.setReturnedDate(null);
+        record.setStatus("ISSUED");
+        return repository.save(record);
+    }
+
+    @Override
+    public IssuedDeviceRecord returnDevice(Long recordld) {
+        IssuedDeviceRecord record = repository.findById(recordld)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Record not found"));
+
+        if (record.getReturnedDate() != null) {
+            throw new BadRequestException("already returned");
+        }
+
+        record.setReturnedDate(LocalDate.now());
+        record.setStatus("RETURNED");
+        return repository.save(record);
+    }
+
+    @Override
+    public List<IssuedDeviceRecord> getlssuedDevicesByEmployee(Long employeeld) {
+        return repository.findByEmployeeld(employeeld);
+    }
+}
