8e0e2798027644b82e33a140c88005a011fc0cf8
bala-amypo <bala@amypo.in>
2025-12-19 10:21:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/IssuedDeviceRecordRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.IssuedDeviceRecord;
+
+public interface IssuedDeviceRecordRepository
+        extends JpaRepository<IssuedDeviceRecord, Long> {
+
+    long countActiveDevicesForEmployee(Long employeeld);
+
+    List<IssuedDeviceRecord> findByEmployeeld(Long employeeld);
+}
