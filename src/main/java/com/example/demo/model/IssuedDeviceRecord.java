4a4e5aee8849d727da5be3595fdd6df495a613c9
bala-amypo <bala@amypo.in>
2025-12-19 10:20:47 +0000

    User commit

--- src/main/java/com/example/demo/model/IssuedDeviceRecord.java
+++ src/main/java/com/example/demo/model/IssuedDeviceRecord.java
@@ -1,63 +1,27 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
-// import java.time.LocalDate;
-// import jakarta.persistence.*;
+import java.time.LocalDate;
 
-// @Entity
-// @Table(name = "issued_device_records")
-// public class IssuedDeviceRecord {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-//     private Long employeeId;
+@Entity
+public class IssuedDeviceRecord {
 
-//     private Long deviceItemId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//     private LocalDate issuedDate;
+    private Long employeeld;
+    private Long deviceltemld;
 
-//     private LocalDate returnedDate;
+    private LocalDate issuedDate;
+    private LocalDate returnedDate;
 
-//     private String status;
+    // ISSUED / RETURNED
+    private String status;
 
-//     @PrePersist
-//     public void onIssue() {
-//         this.issuedDate = LocalDate.now();
-//         this.status = "ISSUED";
-//     }
-
-//     @PreUpdate
-//     public void onReturn() {
-//         if (this.returnedDate != null) {
-//             this.status = "RETURNED";
-//         }
-//     }
-
- 
-//     public Long getId() {
-//          return id; 
-//          }
-//     public Long getEmployeeId() { 
-//         return employeeId;
-//          }
-//     public void setEmployeeId(Long employeeId) {
-//          this.employeeId = employeeId;
-//           }
-//     public Long getDeviceItemId() { 
-//     return deviceItemId; 
-//     }
-//     public void setDeviceItemId(Long deviceItemId) { 
-//         this.deviceItemId = deviceItemId; 
-//         }
-//     public LocalDate getIssuedDate() {
-//          return issuedDate; }
-//     public LocalDate getReturnedDate(){ 
-//         return returnedDate;
-//      }
-//     public void setReturnedDate(LocalDate returnedDate) {
-//          this.returnedDate = returnedDate; 
-//          }
-//     public String getStatus() {
-//          return status; 
-//          }
-// }
+    // getters & setters
+}
