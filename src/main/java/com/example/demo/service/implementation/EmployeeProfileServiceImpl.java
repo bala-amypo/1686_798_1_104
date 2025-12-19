a606de931687bbaf7fd1cb61a702f11c11639faf
bala-amypo <bala@amypo.in>
2025-12-19 09:01:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/implementation/EmployeeProfileServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.EmployeeProfile;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.EmployeeProfileRepository;
+import com.example.demo.service.EmployeeProfileService;
+
+@Service
+public class EmployeeProfileServiceImpl implements EmployeeProfileService {
+
+    private final EmployeeProfileRepository repository;
+
+    public EmployeeProfileServiceImpl(EmployeeProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public EmployeeProfile createEmployee(EmployeeProfile employee) {
+
+        if (repository.findByEmployeeld(employee.getEmployeeId()).isPresent()) {
+            throw new BadRequestException("Employeeld already exists");
+        }
+
+        return repository.save(employee);
+    }
+
+    @Override
+    public EmployeeProfile getEmployeeById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
+    }
+
+    @Override
+    public List<EmployeeProfile> getAllEmployees() {
+        return repository.findAll();
+    }
+
+    @Override
+    public EmployeeProfile updateEmployeeStatus(Long id, boolean active) {
+        EmployeeProfile employee = getEmployeeById(id);
+        employee.setActive(active);
+        return repository.save(employee);
+    }
+}
