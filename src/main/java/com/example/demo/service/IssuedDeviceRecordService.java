b9ed53895b8ae30c1359c825c74d7d4c07ea6fdf
bala-amypo <bala@amypo.in>
2025-12-19 10:25:45 +0000

    User commit

--- src/main/java/com/example/demo/service/IssuedDeviceRecordService.java
+++ src/main/java/com/example/demo/service/IssuedDeviceRecordService.java
@@ -1,52 +1,15 @@
 package com.example.demo.service;
 
-import java.time.LocalDate;
 import java.util.List;
+import com.example.demo.entity.EmployeeProfile;
 
-import org.springframework.stereotype.Service;
+public interface EmployeeProfileService {
 
-import com.example.demo.entity.IssuedDeviceRecord;
-import com.example.demo.exception.BadRequestException;
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.IssuedDeviceRecordRepository;
+    EmployeeProfile createEmployee(EmployeeProfile employee);
 
-@Service
-public class IssuedDeviceRecordService {
+    EmployeeProfile getEmployeeByld(Long id);
 
-    private final IssuedDeviceRecordRepository repository;
+    List<EmployeeProfile> getAllEmployees();
 
-    public IssuedDeviceRecordService(IssuedDeviceRecordRepository repository) {
-        this.repository = repository;
-    }
-
-    // Issue device
-    public IssuedDeviceRecord issueDevice(IssuedDeviceRecord record) {
-
-        record.setIssuedDate(LocalDate.now());
-        record.setReturnedDate(null);
-        record.setStatus("ISSUED");
-
-        return repository.save(record);
-    }
-
-    // Return device
-    public IssuedDeviceRecord returnDevice(Long recordld) {
-
-        IssuedDeviceRecord record = repository.findById(recordld)
-                .orElseThrow(() -> new ResourceNotFoundException("Record not found"));
-
-        if (record.getReturnedDate() != null) {
-            throw new BadRequestException("already returned");
-        }
-
-        record.setReturnedDate(LocalDate.now());
-        record.setStatus("RETURNED");
-
-        return repository.save(record);
-    }
-
-    // Get issued devices by employee
-    public List<IssuedDeviceRecord> getlssuedDevicesByEmployee(Long employeeld) {
-        return repository.findByEmployeeld(employeeld);
-    }
+    EmployeeProfile updateEmployeeStatus(Long id, boolean active);
 }
