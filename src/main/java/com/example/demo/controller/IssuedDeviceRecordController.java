fcf562fcb75d8fa88ff63b324be466f5f2d063e3
bala-amypo <bala@amypo.in>
2025-12-20 04:44:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/IssuedDeviceRecordController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.IssuedDeviceRecord;
+import com.example.demo.service.IssuedDeviceRecordService;
+
+@RestController
+@RequestMapping("/api/issued-devices")
+public class IssuedDeviceRecordController {
+
+    private final IssuedDeviceRecordService service;
+
+    public IssuedDeviceRecordController(IssuedDeviceRecordService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public IssuedDeviceRecord issue(@RequestBody IssuedDeviceRecord record) {
+        return service.issueDevice(record);
+    }
+
+    @PutMapping("/{id}/return")
+    public IssuedDeviceRecord returnDevice(@PathVariable Long id) {
+        return service.returnDevice(id);
+    }
+
+    @GetMapping("/employee/{employeeld}")
+    public List<IssuedDeviceRecord> getByEmployee(@PathVariable Long employeeld) {
+        return service.getIssuedDevicesByEmployee(employeeld);
+    }
+}
