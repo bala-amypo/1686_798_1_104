afc4f9fb156ec11c5e9908f0139ba52c53dd6a49
bala-amypo <bala@amypo.in>
2025-12-21 15:30:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/UserAccount.java
@@ -0,0 +1,39 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+    name = "user_account",
+    uniqueConstraints = @UniqueConstraint(columnNames = "email")
+)
+public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String passwordHash;
+    private String role;
+
+    private Boolean active;
+
+    public Long getId() { return id; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public Boolean getActive() { return active; }
+    public void setActive(Boolean active) { this.active = active; }
+}
