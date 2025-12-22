4f2a20a68e883dc636b626b0c1e8577b1f65605d
bala-amypo <bala@amypo.in>
2025-12-22 04:13:00 +0000

    User commit

--- src/main/java/com/example/demo/model/EligibilityCheckRecord.java
+++ src/main/java/com/example/demo/model/EligibilityCheckRecord.java
@@ -11,8 +11,8 @@ public class EligibilityCheckRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long employeeld;
-    private Long deviceltemld;
+    private Long employeeId;
+    private Long deviceltemId;
     private Boolean isEligible;
     private String reason;
 
@@ -24,8 +24,8 @@ public class EligibilityCheckRecord {
     }
 
     public Long getId() { return id; }
-    public Long getEmployeeld() { return employeeld; }
-    public void setEmployeeld(Long employeeld) { this.employeeld = employeeld; }
+    public Long getEmployeeId() { return employeeId; }
+    public void setEmployeeId(Long employeeId) { this.employeeId = employeeId; }
     public Long getDeviceltemld() { return deviceltemld; }
     public void setDeviceltemld(Long deviceltemld) { this.deviceltemld = deviceltemld; }
     public Boolean getIsEligible() { return isEligible; }
