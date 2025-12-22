8d95e890b2680cdcc48c4ae2c21526fa82c7a5a3
bala-amypo <bala@amypo.in>
2025-12-22 03:38:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/EligibilityCheckController.java
+++ src/main/java/com/example/demo/controller/EligibilityCheckController.java
@@ -1,12 +1,9 @@
 package com.example.demo.controller;
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.*;
-
 import com.example.demo.entity.EligibilityCheckRecord;
 import com.example.demo.service.EligibilityCheckService;
-
 @RestController
 @RequestMapping("/api/eligibility")
 public class EligibilityCheckController {
