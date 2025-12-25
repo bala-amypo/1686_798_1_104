fad852b1b46198844d4624bfafa4c24261daf3d7
bala-amypo <bala@amypo.in>
2025-12-25 13:25:40 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EligibilityCheckServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EligibilityCheckServiceImpl.java
@@ -3,42 +3,6 @@ package com.example.demo.service.impl;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-
-import com.example.demo.entity.EligibilityCheckRecord;
-import com.example.demo.repository.EligibilityCheckRecordRepository;
-import com.example.demo.service.EligibilityCheckService;
-
-@Service
-public class EligibilityCheckServiceImpl implements EligibilityCheckService {
-
-    private final EligibilityCheckRecordRepository repository;
-    public EligibilityCheckServiceImpl(EligibilityCheckRecordRepository repository) {
-        this.repository = repository;
-    }
-`
-    @Override
-    public EligibilityCheckRecord validateEligibility(Long employeeId, Long deviceltemId) {
-
-        EligibilityCheckRecord record = new EligibilityCheckRecord();
-        record.setEmployeeId(employeeId);     
-        record.setDeviceltemId(deviceltemId);
-
-        record.setIsEligible(true);
-        record.setReason("Eligible");
-
-        return repository.save(record);
-    }
-
-    @Override
-    public List<EligibilityCheckRecord> getChecksByEmployee(Long employeeId) {
-        return repository.findByEmployeeId(employeeId);   
-    }
-}
-package com.example.demo.service.impl;
-
-import java.util.List;
-
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.entity.EligibilityCheckRecord;
