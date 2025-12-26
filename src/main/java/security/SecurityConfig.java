84a0990960349771f6304991c3261e7598db0094
bala-amypo <bala@amypo.in>
2025-12-26 09:56:10 +0000

    User commit

--- src/main/java/security/SecurityConfig.java
+++ src/main/java/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity   // 🔥 THIS WAS MISSING
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtFilter;
