1af8fc7ec68061dd638a0e976dd4b353bef8ca05
bala-amypo <bala@amypo.in>
2025-12-26 16:49:05 +0000

    User commit

--- src/main/java/com/example/demo/service/PolicyRuleService.java
+++ src/main/java/com/example/demo/service/PolicyRuleService.java
@@ -11,3 +11,14 @@
 
 //     List<PolicyRule> getActiveRules();
 // }
+
+package com.example.demo.service;
+
+import com.example.demo.model.PolicyRule;
+import java.util.List;
+
+public interface PolicyRuleService {
+    PolicyRule createRule(PolicyRule rule);
+    List<PolicyRule> getAllRules();
+    List<PolicyRule> getActiveRules();
+}
