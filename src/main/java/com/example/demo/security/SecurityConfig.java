93b32039b3d388b3169456c8d8c9b49df9ac7c7e
bala-amypo <bala@amypo.in>
2025-12-26 18:42:02 +0000

    User commit

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -26,6 +26,39 @@
 // }
 
 
+// package com.example.demo.security;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
+
+// @Configuration
+// public class SecurityConfig {
+
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+//         http
+//             // Disable CSRF for APIs & Swagger
+//             .csrf(csrf -> csrf.disable())
+
+//             // Allow Swagger and all endpoints
+//             .authorizeHttpRequests(auth -> auth
+//                 .requestMatchers(
+//                     "/v3/api-docs/**",
+//                     "/swagger-ui/**",
+//                     "/swagger-ui.html"
+//                 ).permitAll()
+//                 .anyRequest().permitAll()
+//             );
+
+//         return http.build();
+//     }
+// }
+
+
+
 package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
@@ -40,10 +73,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            // Disable CSRF for APIs & Swagger
             .csrf(csrf -> csrf.disable())
-
-            // Allow Swagger and all endpoints
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                     "/v3/api-docs/**",
@@ -56,6 +86,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
-
-
-
