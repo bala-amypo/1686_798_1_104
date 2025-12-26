e8f84184a9b2f155e4fb3dcc83ab13724ae6043e
bala-amypo <bala@amypo.in>
2025-12-26 09:02:09 +0000

    User commit

--- src/main/java/com/example/demo/service/IssuedDeviceRecordService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.IssuedDeviceRecord;
-
-public interface IssuedDeviceRecordService {
-    IssuedDeviceRecord returnDevice(Long id);
-}
