c2056785a7fdd45576a8ab94f562470c9c89400f
bala-amypo <bala@amypo.in>
2025-12-27 15:49:04 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/IssuedDeviceRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/IssuedDeviceRecordServiceImpl.java
@@ -107,22 +107,3 @@
 //     }
 // }
 
-package com.example.demo.service;
-
-import java.util.List;
-import com.example.demo.model.IssuedDeviceRecord;
-
-public interface IssuedDeviceRecordService {
-
-    IssuedDeviceRecord issueDevice(IssuedDeviceRecord record);
-
-    IssuedDeviceRecord returnDevice(Long recordId);
-
-    List<IssuedDeviceRecord> getByEmployeeId(Long employeeId);
-
-    List<IssuedDeviceRecord> getActiveByEmployeeId(Long employeeId);
-
-    IssuedDeviceRecord getById(Long id);
-
-    long countActiveDevicesForEmployee(Long employeeId);
-}
--- src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PolicyRuleServiceImpl.java
@@ -73,22 +73,3 @@
 //     }
 // }
 
-package com.example.demo.service;
-
-import java.util.List;
-
-import com.example.demo.model.PolicyRule;
-
-public interface PolicyRuleService {
-
-    PolicyRule createRule(PolicyRule rule);
-
-    List<PolicyRule> getAllRules();
-
-    List<PolicyRule> getActiveRules();
-
-    PolicyRule updateRuleActive(Long id, boolean active);
-
-    void deleteRule(Long id);
-}
-
