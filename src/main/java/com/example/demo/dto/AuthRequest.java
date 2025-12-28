d063c49a269ed7411505368979aa17d9fd4ca378
bala-amypo <bala@amypo.in>
2025-12-28 11:46:28 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -25,18 +25,18 @@
 //     }
 // }
 
-package com.example.demo.dto;
+// package com.example.demo.dto;
 
-public class AuthRequest {
-    private String email;
-    private String password;
+// public class AuthRequest {
+//     private String email;
+//     private String password;
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+//     public String getEmail() { return email; }
+//     public void setEmail(String email) { this.email = email; }
     
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-}
+//     public String getPassword() { return password; }
+//     public void setPassword(String password) { this.password = password; }
+// }
 
 // package com.example.demo.dto;
 
@@ -63,3 +63,48 @@ public class AuthRequest {
 //         this.password = password;
 //     }
 // }
+
+package com.example.demo.dto;
+
+public class AuthRequest {
+
+    private String fullName;   // register
+    private String email;      // register + login
+    private String password;   // register
+    private String role;       // register
+
+    public AuthRequest() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
