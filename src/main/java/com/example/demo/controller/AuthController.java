b3881d5c5a15ad244371128b6b542a339d5646b4
bala-amypo <bala@amypo.in>
2025-12-28 11:34:28 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -45,42 +45,42 @@
 //     }
 // }
 
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import com.example.demo.dto.AuthRequest;
-// import com.example.demo.dto.AuthResponse;
-// import com.example.demo.model.UserAccount;
-// import com.example.demo.security.JwtTokenProvider;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.model.UserAccount;
+import com.example.demo.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-// @RestController
-// @RequestMapping("/api/auth")
-// public class AuthController {
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
 
-//      @Autowired
-//      private JwtTokenProvider tokenProvider;
+     @Autowired
+     private JwtTokenProvider tokenProvider;
 
-//     @PostMapping("/login")
-//     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-//         UserAccount user = new UserAccount();
-//         user.setId(1L);
-//         user.setEmail(request.getEmail());
-//         user.setRole("USER");
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        UserAccount user = new UserAccount();
+        user.setId(1L);
+        user.setEmail(request.getEmail());
+        user.setRole("USER");
         
-//         String token = tokenProvider.generateToken(user);
+        String token = tokenProvider.generateToken(user);
         
-//         AuthResponse response = new AuthResponse();
-//         response.setToken(token);
-//         response.setEmail(user.getEmail());
-//         response.setRole(user.getRole());
+        AuthResponse response = new AuthResponse();
+        response.setToken(token);
+        response.setEmail(user.getEmail());
+        response.setRole(user.getRole());
         
-//         return ResponseEntity.ok(response);
-//     }
+        return ResponseEntity.ok(response);
+    }
 
 
-// }
+}
 
 // package com.example.demo.controller;
 
